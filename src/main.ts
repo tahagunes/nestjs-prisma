1c0c85eee9c4410722d1cd2e99518507aa6e41c2
Osman Taha Güneş <ttahaa70@gmail.com>
2023-10-16 00:20:33 +0300

    #3 error-handling

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
-import { NestFactory, Reflector } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe , ClassSerializerInterceptor} from '@nestjs/common';
+import { AppModule } from './app.module';
+import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // binds ValidationPipe to the entire application
   app.useGlobalPipes(new ValidationPipe());
    // 👇 apply transform to all responses
    app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+   // 👇 apply PrismaClientExceptionFilter to entire application, requires HttpAdapterHost because it extends BaseExceptionFilter
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
   const config = new DocumentBuilder()
     .setTitle('NestJS Prisma Workshop')
     .setDescription('API description')
